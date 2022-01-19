2ba43741ba486d32a80ef2892070326549e5a93d
HOME <HOME@10.22.10.2>
2022-01-19 13:40:27 +0900

    두번째 커밋

--- src/com/itwill/shop/Member.java
+++ src/com/itwill/shop/Member.java
@@ -1,5 +1,19 @@
 package com.itwill.shop;
 
 public class Member {
+		private int no;
 
+		public int getNo() {
+			return no;
+		}
+
+		public void setNo(int no) {
+			this.no = no;
+		}
+
+		public Member(int no) {
+			super();
+			this.no = no;
+		}
+		
 }

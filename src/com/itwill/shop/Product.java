64c1289643014a4e645be8081506d156772f23b4
HOME <HOME@10.22.10.2>
2022-01-19 13:41:48 +0900

    세번째 커밋

--- src/com/itwill/shop/Product.java
+++ src/com/itwill/shop/Product.java
@@ -1,5 +1,19 @@
 package com.itwill.shop;
 
 public class Product {
+	private int no;
+
+	public Product(int no) {
+		super();
+		this.no = no;
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
 
 }
